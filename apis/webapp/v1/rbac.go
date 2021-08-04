9071972a1255f8e692ba2c7cd1dcbd594c23f69a
Shengyu Liu <usungu@gmail.com>
2021-08-04 22:20:20 +0800

    Add rbac

---
+++ apis/webapp/v1/rbac.go
@@ -0,0 +1,4 @@
+// +kubebuilder:rbac:groups=webapp.shengyu.io,resources=guestbooks,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=webapp.shengyu.io,resources=guestbooks/status,verbs=get;update;patch
+
+package v1
